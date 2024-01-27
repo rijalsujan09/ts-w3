81b50539bb07664afec933c82df4b1443228cba6
rijalsujan09 <srijal@castera.io>
2024-01-27 16:30:58 -0500

    wrapping up  TS from w3 school

--- 17updates/index.ts
+++ 17updates/index.ts
@@ -7,3 +7,10 @@ type HexColor<T extends Color > = `#${string}`;
 let myColor : HexColor<"blue"> = "#0000FF";
 
 console.log(myColor);
+
+
+// Index Signature labels
+
+// type DynamicObject = { [key: string as `dynamic_${string}`]: string };
+
+// let obj: DynamicObject = { dynamic_key: "value" };
